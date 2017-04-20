a446900c1e9c13c506aca284562a6b382b9eb65b
iskolbin <iskolbin@gmail.com>
2017-04-20 17:21:45 +0300

    fixes; refactorings+renamings; finished tests;100% coverage

--- src/Ratio.ts
+++ src/Ratio.ts
@@ -1,22 +1,34 @@
 function gcd( a: number, b: number ): number {
-	while ( a !== b ) {
-		if ( a > b ) {
-			a -= b
-		} else {
-			b -= a
+	if ( a === b ) {
+		return a
+	} else if ( a > b && a % b === 0) {
+		return b
+	} else if ( b > a && b % a === 0) {
+		return a
+	} else {
+		let c = b
+		while ( b !== 0 ) {
+			c = b
+			b = a % b
+			a = c
 		}
+		return c
 	}
-	return a
 }
 
 export class Ratio {
 	static ERROR_NUMERATOR_NAN = 'Ratio numerator is NaN'
 	static ERROR_DENOMINATOR_NAN = 'Ratio denominator is NaN'
+	static ERROR_ARGUMENTS_NAN = 'Ratio numerator and denominator are NaN'
 	static ERROR_NUMERATOR_NOT_INTEGER = 'Ratio numerator is not an integer'
 	static ERROR_DENOMINATOR_NOT_INTEGER = 'Ratio denominator is not an integer'
-	static ERROR_DENOMINATOR_IS_ZERO = 'Ratio denominator is zero'
+	static ERROR_ARGUMENTS_NOT_INTEGER = 'Ratio numerator and denominator are not integers'
+	static ERROR_DENOMINATOR_ZERO = 'Ratio denominator is zero'
 	static ERROR_DIVISION_BY_ZERO = 'Ratio divided by ratio with zero numerator'
-	static ERROR_BAD_CONSTRUCTOR_ARGUMENTS = 'Bad arguments passed to parse method'
+	static ERROR_BAD_PARSE_ARGUMENTS = 'Bad arguments passed to parse method'
+	static ERROR_PARSE_OVERFLOW = 'Overflow during parse'
+
+	static NUMBER_LIMIT = Math.pow( 2, 53 )
 
 	readonly _num: number
 	readonly _den: number
@@ -25,15 +37,23 @@ export class Ratio {
 
 		if ( !noChecks ) {
 			if ( num !== num ) {
-				throw new Error( Ratio.ERROR_NUMERATOR_NAN )
+				if ( den !== den ) {
+					throw new Error( Ratio.ERROR_ARGUMENTS_NAN )
+				} else {
+					throw new Error( Ratio.ERROR_NUMERATOR_NAN )
+				}
 			} else if ( den !== den ) {
 				throw new Error( Ratio.ERROR_DENOMINATOR_NAN )
 			} else if ( Math.floor( num ) !== num ) {
-				throw new Error( Ratio.ERROR_NUMERATOR_NOT_INTEGER )
+				if ( Math.floor( den ) !== den ) {
+					throw new Error( Ratio.ERROR_ARGUMENTS_NOT_INTEGER )
+				} else {
+					throw new Error( Ratio.ERROR_NUMERATOR_NOT_INTEGER )
+				}
 			} else if ( Math.floor( den ) !== den ) {
 				throw new Error( Ratio.ERROR_DENOMINATOR_NOT_INTEGER )
 			} else if ( den === 0 ) {
-				throw new Error( Ratio.ERROR_DENOMINATOR_IS_ZERO )
+				throw new Error( Ratio.ERROR_DENOMINATOR_ZERO )
 			}
 		}
 
@@ -89,7 +109,7 @@ export class Ratio {
 
 	lt( {_num,_den}: Ratio ): boolean {
 		if ( this._den === _den ) {
-			return this._num < this._den
+			return this._num < _num
 		} else {
 			return this._num * _den < _num * this._den
 		}
@@ -111,14 +131,6 @@ export class Ratio {
 		return !this.lt( ratio )
 	}
 
-	toInteger(): number {
-		return this._num < 0 ? Math.ceil( this._num/this._den ) : Math.floor( this._num/this._den )
-	}
-
-	toNumber(): number {
-		return this._num / this._den
-	}
-
 	toString(): string {
 		if ( this._num === 0 ) {
 			return 'Ratio (0)'
@@ -130,7 +142,7 @@ export class Ratio {
 	}
 
 	valueOf(): number {
-		return this.toNumber()
+		return this._num / this._den
 	}
 
 	get num(): number {
@@ -142,18 +154,22 @@ export class Ratio {
 	}
 
 	get int(): Ratio {
-		return new Ratio( this.toInteger(), 1, true )
+		const i = this._num < 0 ? Math.ceil( this._num/this._den ) : Math.floor( this._num/this._den )
+		return new Ratio( i, 1, true )
 	}
 
 	get frac(): Ratio {
-		return this.sub( new Ratio( this.toInteger(), 1, true ))
+		return this.sub( this.int )
 	} 
 	
-	static parse( s: number|string ): Ratio {
-		const n = typeof s === 'string' ? parseFloat( s ) : s	
+	static parse( s: number|string, overflowLimit = Ratio.NUMBER_LIMIT ): Ratio {
+		const n = typeof s === 'number' ? s : parseFloat( s )
 		if ( n !== n ) {
-			throw new Error( Ratio.ERROR_BAD_CONSTRUCTOR_ARGUMENTS )
+			throw new Error( Ratio.ERROR_BAD_PARSE_ARGUMENTS )
 		} else if ( Math.floor( n ) === n ) {
+			if ( n > overflowLimit ) {
+				throw new Error( Ratio.ERROR_PARSE_OVERFLOW )
+			}
 			return new Ratio( n, 1, true )
 		} else {
 			const positive = n > 0
@@ -165,6 +181,9 @@ export class Ratio {
 				num1 *= 10
 				den2 *= 10
 				den1 *= 10
+				if ( num1 > overflowLimit || den2 > overflowLimit || den1 > overflowLimit ) {
+					throw new Error( Ratio.ERROR_PARSE_OVERFLOW )
+				}
 			}
 			if ( Math.floor(num1) == num1 ) {
 				return new Ratio( positive ? num1 : -num1, den1, true )
--- test.ts
+++ test.ts
@@ -1,6 +1,16 @@
+import "blanket"
 import { Ratio } from './src/Ratio'
 import { suite, test, slow, timeout } from "mocha-typescript"
-import { deepEqual, throws } from "assert"
+import { assert, deepEqual, throws } from "assert"
+
+const throwsMessage = ( f: () => void, errorMessage: string ) => {
+	try {
+		f()
+		throw new Error()
+	} catch ( {message} ) {
+		deepEqual( errorMessage, message )
+	}
+}
 
 @suite class RatioTestSute {
 	@test("constructor without args returns ratio 0/1")
@@ -26,37 +36,46 @@ import { deepEqual, throws } from "assert"
 		deepEqual( new Ratio( 59, 151 ), {_num: 59, _den: 151} )
 	}
 
-	@test("constructor with non-integer args throws error")
-	ConstructorNoIntegerArgs() {
-		throws( () => { new Ratio( 5.6 ) } )
-		throws( () => { new Ratio( 3.2, 4.5 ) } )
-		throws( () => { new Ratio( 5.6, 4 ) } )
-		throws( () => { new Ratio( 3, 5.6 ) } )
-		throws( () => { new Ratio( 0, 5.1 ) } )
+	@test("constructor always puts sign in the numerator")
+	SignInNumerator() {
+		deepEqual( new Ratio( 1, -23 ), {_num: -1, _den: 23} )
+		deepEqual( new Ratio( -2, -3 ), {_num: 2, _den: 3} )
+		deepEqual( new Ratio( -1, 2 ), {_num: -1, _den: 2} )
 	}
 
-	@test("constructor with second arg === 0 throws error")
+	@test("constructor with non-integer args throws error")
+	ConstructorNoIntegerArgs() {
+		throwsMessage( () => { new Ratio( 5.6 ) }, Ratio.ERROR_NUMERATOR_NOT_INTEGER )
+		throwsMessage( () => { new Ratio( 3.2, 4.5 ) }, Ratio.ERROR_ARGUMENTS_NOT_INTEGER )
+		throwsMessage( () => { new Ratio( 5.6, 4 ) }, Ratio.ERROR_NUMERATOR_NOT_INTEGER )
+		throwsMessage( () => { new Ratio( 3, 5.6 ) }, Ratio.ERROR_DENOMINATOR_NOT_INTEGER )
+		throwsMessage( () => { new Ratio( 0, 5.1 ) }, Ratio.ERROR_DENOMINATOR_NOT_INTEGER )
+	}
+
+	@test("constructor with second arg (denominatior) === 0 throws error")
 	ConstructorZeroSecondArg() {
-		throws( () => { new Ratio( 0, 0 ) } )
-		throws( () => { new Ratio( 51, 0 ) } )
-		throws( () => { new Ratio( 31, 0 ) } )
+		throwsMessage( () => { new Ratio( 0, 0 ) }, Ratio.ERROR_DENOMINATOR_ZERO )
+		throwsMessage( () => { new Ratio( 51, 0 ) }, Ratio.ERROR_DENOMINATOR_ZERO )
+		throwsMessage( () => { new Ratio( 31, 0 ) }, Ratio.ERROR_DENOMINATOR_ZERO )
 	}
 
 	@test("constructor with NaN args throws error")
 	ConstructorNaNArgs() {
-		throws( () => { new Ratio( NaN ) } )
-		throws( () => { new Ratio( 1, NaN ) } )
-		throws( () => { new Ratio( NaN, NaN ) } )
+		throwsMessage( () => { new Ratio( NaN ) }, Ratio.ERROR_NUMERATOR_NAN )
+		throwsMessage( () => { new Ratio( 1, NaN ) }, Ratio.ERROR_DENOMINATOR_NAN )
+		throws( () => { new Ratio( NaN, NaN ) }, Ratio.ERROR_ARGUMENTS_NAN )
 	}
 
 	@test("adding two ratios")
 	Add() {
 		deepEqual( new Ratio( 1, 2 ).add( new Ratio( 2, 3 )), new Ratio( 7, 6 ))
+		deepEqual( new Ratio( 1, 2 ).add( new Ratio( 1, 2 )), new Ratio( 1 ))
 	}
 
 	@test("subtracting two ratios")
 	Subtract() {
 		deepEqual( new Ratio( 1, 2 ).sub( new Ratio( 1, 3 )), new Ratio( 1, 6 ))
+		deepEqual( new Ratio( 1, 2 ).sub( new Ratio( 1, 2 )), new Ratio( 0 ))
 	}
 
 	@test("multiplying two ratios")
@@ -71,11 +90,193 @@ import { deepEqual, throws } from "assert"
 
 	@test("dividing by zero throws error")
 	DivideByZero() {
-		throws( () => { new Ratio( 5, 3 ).div( new Ratio( 0, 2 )) } )
+		throwsMessage( () => new Ratio().div( new Ratio()), Ratio.ERROR_DIVISION_BY_ZERO )
+		throwsMessage( () => { new Ratio( 7, 1 ).div( new Ratio( 0 )) }, Ratio.ERROR_DIVISION_BY_ZERO )
+		throwsMessage( () => { new Ratio( 5, 3 ).div( new Ratio( 0, 2 )) }, Ratio.ERROR_DIVISION_BY_ZERO )
 	}
 
 	@test("negation")
 	Negation() {
 		deepEqual( new Ratio( 12, 34 ).neg(), new Ratio( -12, 34 ) )
+	}
+
+	@test("normalization")
+	Normalization() {
+		deepEqual( new Ratio( 1, 2 ), new Ratio( 23, 46 ))
+		deepEqual( new Ratio( 5, 9 ), new Ratio( 25, 45 ))
+		deepEqual( new Ratio( 5 ), new Ratio( 10000, 2000 ))
+	}
+
+	@test("equal")
+	Equal() {
+		deepEqual( new Ratio( 1, 2 ).eq( new Ratio( 4, 8 )), true )
+		deepEqual( new Ratio( 1, 3 ).eq( new Ratio( 4, 8 )), false )
+	}
+
+	@test("not equal")
+	NotEqual() {
+		deepEqual( new Ratio( 3, 7 ).ne( new Ratio( 3, 8 )), true )
+		deepEqual( new Ratio( 3, 8 ).ne( new Ratio( 3, 8 )), false )
+	}
+
+	@test("less or equal")
+	LessOrEqual() {
+		deepEqual( new Ratio( 3, 7 ).le( new Ratio( 6, 14 )), true )
+		deepEqual( new Ratio( 3, 7 ).le( new Ratio( 4, 7 )), true )
+		deepEqual( new Ratio( 3, 7 ).le( new Ratio( 2, 7 )), false )
+	}
+
+	@test("less")
+	Less() {
+		deepEqual( new Ratio( 3, 7 ).lt( new Ratio( 6, 14 )), false )
+		deepEqual( new Ratio( 3, 7 ).lt( new Ratio( 4, 7 )), true )
+		deepEqual( new Ratio( 3, 7 ).lt( new Ratio( 2, 7 )), false )
+	}
+
+	@test("greater")
+	Greater() {
+		deepEqual( new Ratio( 12, 24 ).gt( new Ratio( 6, 12 )), false )
+		deepEqual( new Ratio( 2, 6 ).gt( new Ratio( 1, 5 )), true )
+		deepEqual( new Ratio( 1, 6 ).gt( new Ratio( 2, 8 )), false )
+	}
+	
+	@test("greater or equal")
+	GreaterOrEqual() {
+		deepEqual( new Ratio( 12, 24 ).ge( new Ratio( 6, 12 )), true )
+		deepEqual( new Ratio( 2, 6 ).ge( new Ratio( 1, 5 )), true )
+		deepEqual( new Ratio( 1, 6 ).ge( new Ratio( 2, 8 )), false )
+	}
+
+	@test("conversion to primitive value")
+	ValueOf() {
+		deepEqual( new Ratio( 5, 2 ).valueOf(), 2.5 )
+		deepEqual( new Ratio( 2, 100 ).valueOf(), 0.02 )
+		deepEqual( new Ratio( 1 ).valueOf(), 1 )
+		deepEqual( new Ratio().valueOf(), 0 )
+		deepEqual( new Ratio( 125, 25 ).valueOf(), 5 )
+	}
+
+	@test("comparsion with primitive values")
+	ValueCompare() {
+		deepEqual( (<any>new Ratio( 5, 2 )) == 2.5, true )
+		deepEqual( (<any>new Ratio( 2, 100 )) == 0.02, true )
+		deepEqual( (<any>new Ratio( 1 )) == 1, true )
+		deepEqual( (<any>new Ratio()) == 0, true )
+		deepEqual( (<any>new Ratio( 125, 25 )) == 5, true )
+		const r: any = new Ratio( 5, 2 )
+		deepEqual( r < 2.5, false )
+		deepEqual( r <= 2.5, true )
+		deepEqual( r < 3, true )
+		deepEqual( r <= 3.5, true )
+		deepEqual( r == 3.5, false )
+		deepEqual( r == 2.5, true )
+		deepEqual( r != 3.5, true )
+		deepEqual( r > 2.5, false )
+		deepEqual( r >= 2.5, true )
+		deepEqual( r > 1, true )
+		deepEqual( r >= 1, true )
+		deepEqual( r >= 3.5, false )
+	}
+
+	@test("conversion to string")
+	ConvertToString() {
+		deepEqual( new Ratio().toString(), "Ratio (0)" )
+		deepEqual( new Ratio(5).toString(), "Ratio (5)" )
+		deepEqual( new Ratio(5,2).toString(), "Ratio (5/2)" )
+		deepEqual( new Ratio(0,1411).toString(), "Ratio (0)" )
+		deepEqual( new Ratio(5,25).toString(), "Ratio (1/5)" )
+	}
+
+	@test("numerator getter")
+	NumeratorGetter() {
+		deepEqual( new Ratio().num, 0 )
+		deepEqual( new Ratio(1).num, 1 )
+		deepEqual( new Ratio( 5, 10 ).num, 1 )
+		deepEqual( new Ratio( 5, 2 ).num, 5 )
+	}
+
+	@test("denominator getter")
+	DenominatorGetter() {
+		deepEqual( new Ratio().den, 1 )
+		deepEqual( new Ratio( 1 ).den, 1 )
+		deepEqual( new Ratio( 5, 2 ).den, 2 )
+		deepEqual( new Ratio( 123, 321 ).den, 107 )
+	}
+
+	@test("integral part getter")
+	IntegralPartGetter() {
+		deepEqual( new Ratio().int, new Ratio() )
+		deepEqual( new Ratio( 5 ).int, new Ratio( 5 ))
+		deepEqual( new Ratio( 5, 2 ).int, new Ratio( 2 ))
+		deepEqual( new Ratio( -5, 2 ).int, new Ratio( -2 ))
+		deepEqual( new Ratio( 1, 2 ).int, new Ratio( 0 ))
+	}
+
+	@test("fractional part getter")
+	FractionalPartGetter() {
+		deepEqual( new Ratio().frac, new Ratio())
+		deepEqual( new Ratio( 1, 2 ).frac, new Ratio( 1, 2 ))
+		deepEqual( new Ratio( 3, 2 ).frac, new Ratio( 1, 2 ))
+		deepEqual( new Ratio( 5, 3 ).frac, new Ratio( 2, 3 ))
+		deepEqual( new Ratio( -5, 3 ).frac, new Ratio( -2, 3 ))
+	}
+
+	@test("integral and fractional parts")
+	IntegralAndFractionalParts() {
+		const r = new Ratio( 5, 3 )
+		deepEqual( r.int.add( r.frac ), r )
+		const r2 = new Ratio( -5, 3 )
+		deepEqual( r2.int.add( r2.frac ), r2 )
+	}
+
+	@test("parse numbers")
+	ParseNumbers() {
+		deepEqual( Ratio.parse( 11 ), new Ratio( 11 ))
+		deepEqual( Ratio.parse( 102.1 ), new Ratio( 1021, 10 ))
+		deepEqual( Ratio.parse( 2.52 ), new Ratio( 126, 50 ))
+		deepEqual( Ratio.parse( 0.1 ), new Ratio( 1, 10 ))
+		deepEqual( Ratio.parse( -13.12 ), new Ratio( -1312, 100 ))
+		deepEqual( Ratio.parse( -0.2 ), new Ratio( 2, -10 ))
+	}
+
+	@test("parse strings")
+	ParseStrings() {
+		deepEqual( Ratio.parse( "11" ), new Ratio( 11 ))
+		deepEqual( Ratio.parse( "102.1" ), new Ratio( 1021, 10 ))
+		deepEqual( Ratio.parse( "2.52" ), new Ratio( 126, 50 ))
+		deepEqual( Ratio.parse( "0.1" ), new Ratio( 1, 10 ))
+		deepEqual( Ratio.parse( "+2.1" ), new Ratio( 21, 10 ))
+		deepEqual( Ratio.parse( "-3.1" ), new Ratio( -31, 10 ))
+	}
+
+	@test("bad arguments to parse throws error")
+	BadParse() {
+		throwsMessage( () => Ratio.parse( NaN ), Ratio.ERROR_BAD_PARSE_ARGUMENTS )
+		throwsMessage( () => Ratio.parse( "" ), Ratio.ERROR_BAD_PARSE_ARGUMENTS )
+		throwsMessage( () => Ratio.parse( "zeawe" ), Ratio.ERROR_BAD_PARSE_ARGUMENTS )
+		let x: any
+		throwsMessage( () => Ratio.parse( x ), Ratio.ERROR_BAD_PARSE_ARGUMENTS )
+		x = null
+		throwsMessage( () => Ratio.parse( x ), Ratio.ERROR_BAD_PARSE_ARGUMENTS )
+		x = undefined
+		throwsMessage( () => Ratio.parse( x ), Ratio.ERROR_BAD_PARSE_ARGUMENTS )
+		x = {}
+		throwsMessage( () => Ratio.parse( x ), Ratio.ERROR_BAD_PARSE_ARGUMENTS )
+		x = []
+		throwsMessage( () => Ratio.parse( x ), Ratio.ERROR_BAD_PARSE_ARGUMENTS )
+		x = () => {}
+		throwsMessage( () => Ratio.parse( x ), Ratio.ERROR_BAD_PARSE_ARGUMENTS )
+	}
+
+	@test("too large values causes parse to throw overflow error")
+	ParseOverflow() {
+		throwsMessage( () => Ratio.parse( Math.pow(2,60)), Ratio.ERROR_PARSE_OVERFLOW )
+		throwsMessage( () => Ratio.parse( "100000000000000000000" ), Ratio.ERROR_PARSE_OVERFLOW )
+		throwsMessage( () => Ratio.parse( 124123.12315123124321512 ), Ratio.ERROR_PARSE_OVERFLOW )
+	}
+
+	@test("custom overflow value")
+	ParseCustomOverflow() {
+		throwsMessage( () => Ratio.parse( "1000", 999 ), Ratio.ERROR_PARSE_OVERFLOW )
 	}
 }
